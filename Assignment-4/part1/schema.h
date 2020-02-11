ab891b5070e467e2044a699e15e52c21259182d7
Benjamin Kosiborod <bendavp@Bennys-MacBook-Pro.local>
2020-02-11 18:41:44 -0500

    moved schema out of dataframe.h

---
+++ Assignment-4/part1/schema.h
@@ -0,0 +1,74 @@
+//lang:Cwc
+
+#include "fastarray.h"
+/*************************************************************************
+ * Schema::
+ * A schema is a description of the contents of a data frame, the schema
+ * knows the number of columns and number of rows, the type of each column,
+ * optionally columns and rows can be named by strings.
+ * The valid types are represented by the chars 'S', 'B', 'I' and 'F'.
+ */
+class Schema : public Object
+{
+public:
+    StrBuff coltypes_;
+    StringFastArray row_names_;
+    StringFastArray col_names_;
+
+    /** Copying constructor */
+    Schema(Schema &from)
+    {
+        coltypes_ = from.coltypes_;
+        row_names_ = from.row_names_;
+        col_names_ = from.col_names_;
+    }
+
+    /** Create an empty schema **/
+    Schema()
+    {
+        coltypes_ = new StrBuff();
+        row_names_ = new StringFastArray();
+        col_names_ = new StringFastArray();
+    }
+
+    /** Create a schema from a string of types. A string that contains
+    * characters other than those identifying the four type results in
+    * undefined behavior. The argument is external, a nullptr argument is
+    * undefined. **/
+    Schema(const char *types)
+    {
+        coltypes_ = new StrBuff(types);
+    }
+
+    /** Add a column of the given type and name (can be nullptr), name
+    * is external. Names are expectd to be unique, duplicates result
+    * in undefined behavior. */
+    void add_column(char typ, String *name) {}
+
+    /** Add a row with a name (possibly nullptr), name is external.  Names are
+   *  expectd to be unique, duplicates result in undefined behavior. */
+    void add_row(String *name) {}
+
+    /** Return name of row at idx; nullptr indicates no name. An idx >= width
+    * is undefined. */
+    String *row_name(size_t idx) {}
+
+    /** Return name of column at idx; nullptr indicates no name given.
+    *  An idx >= width is undefined.*/
+    String *col_name(size_t idx) {}
+
+    /** Return type of column at idx. An idx >= width is undefined. */
+    char col_type(size_t idx) {}
+
+    /** Given a column name return its index, or -1. */
+    int col_idx(const char *name) {}
+
+    /** Given a row name return its index, or -1. */
+    int row_idx(const char *name) {}
+
+    /** The number of columns */
+    size_t width() {}
+
+    /** The number of rows */
+    size_t length() {}
+};
