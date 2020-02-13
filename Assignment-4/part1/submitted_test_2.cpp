c43b26ad48a9f05e0516e35676725abf01b8dfce
Amy Tang <tang.amy@husky.neu.edu>
2020-02-13 02:33:55 -0500

    adding some tests

--- Assignment-4/part1/submitted_test_2.cpp
+++ Assignment-4/part1/submitted_test_2.cpp
@@ -1,1 +1,18 @@
-// fielder
+//lang CwC
+// testing column
+
+#include <gtest/gtest.h>
+#include "dataframe.h"
+
+#define CS4500_ASSERT_TRUE(a) \
+    ASSERT_EQ((a), true);
+#define CS4500_ASSERT_FALSE(a) \
+    ASSERT_EQ((a), false);
+#define CS4500_ASSERT_EXIT_ZERO(a) \
+    ASSERT_EXIT(a(), ::testing::ExitedWithCode(0), ".*");
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
