1f4accd34cc4bb9156ae5b93aea8187db43d2a43
Amy Tang <tang.amy@husky.neu.edu>
2020-02-14 13:07:59 -0500

    for merging

---
+++ Assignment-4/part1/submitted_test_1.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "dataframe.h"
+
+#define CS4500_ASSERT_TRUE(a) \
+    ASSERT_EQ((a), true);
+#define CS4500_ASSERT_FALSE(a) \
+    ASSERT_EQ((a), false);
+#define CS4500_ASSERT_EXIT_ZERO(a) \
+    ASSERT_EXIT(a(), ::testing::ExitedWithCode(0), ".*");
+
+/**
+ * @brief Runs all Google Tests in this file
+ */
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
